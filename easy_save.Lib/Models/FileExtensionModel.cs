3b6b09ac1c4b5d2ea136817eda21745da114de67
HeyTigrou <gautier.allias@viacesi.fr>
2023-02-14 17:19:21 +0100

    ajout model file extension

--- easy_save.Lib/Models/FileExtensionModel.cs
+++ easy_save.Lib/Models/FileExtensionModel.cs
@@ -4,9 +4,62 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace easy_save.Lib.Models
+namespace DetectSoftware
 {
-    internal class FileExtensionModel
+
+    public class FileExtensionModel
     {
+        public List<string> Extensions { get; set; }
+
+        public List<string> SelectedExtensions { get; set; }
+
+        private static FileExtensionModel instance;
+
+        private FileExtensionModel()
+        {
+
+        }
+
+        public static FileExtensionModel Instance
+        {
+            get
+            {
+                if (instance == null)
+                {
+                    instance = new FileExtensionModel();
+                    instance.Extensions = new List<string> {
+                        ".txt",
+                        ".doc",
+                        ".docx",
+                        ".pdf",
+                        ".xls",
+                        ".xlsx",
+                        ".ppt",
+                        ".pptx",
+                        ".jpg",
+                        ".jpeg",
+                        ".png",
+                        ".gif",
+                        ".bmp",
+                        ".mp3",
+                        ".wav",
+                        ".mp4",
+                        ".avi",
+                        ".wmv",
+                        ".mov",
+                        ".zip",
+                        ".rar",
+                        ".7z",
+                        ".exe",
+                        ".dll",
+                        ".iso",
+                        ".bat",
+                        ".cmd"
+                    };
+                    instance.SelectedExtensions = new List<string> { };
+                }
+                return instance;
+            }
+        }
     }
 }
