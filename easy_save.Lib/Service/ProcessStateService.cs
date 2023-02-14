8686458ad88a4fb557d55ee8e0b204333f537a03
HeyTigrou <gautier.allias@viacesi.fr>
2023-02-14 23:00:24 +0100

    Ajout Process State service

---
+++ easy_save.Lib/Service/ProcessStateService.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace easy_save.Lib.Service
+{
+    internal class ProcessStateService
+    {
+    }
+}
