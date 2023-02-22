8984ff527129e002eeac598a5cf3dea79915a26a
LaTrivs <leotrivino.pro@gmail.com>
2023-02-22 10:59:06 +0100

    Ajout de la Console déportée

---
+++ easy_save.Client/Connexion.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Sockets;
+using System.Net;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace easy_save.Client
+{
+    internal class Connexion
+    {
+        public static void Main(string[] args)
+        {
+            using (Socket socket = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp))
+            {
+                IPEndPoint server = new IPEndPoint(IPAddress.Parse("127.0.0.1"), 55894);
+                socket.Connect(server);
+
+                Console.WriteLine($"Client connected to {server.Address}:{server.Port}");
+
+                // Message de bienvenue
+                byte[] buffer = new byte[1024];
+                int count = socket.Receive(buffer);
+                Console.WriteLine(Serializer.Deserialize(buffer, count));
+
+                Console.WriteLine("Tap quit to exit");
+
+                do
+                {
+                    string message = Console.ReadLine();
+                    if (message == "quit")
+                        break;
+
+                    try { socket.Send(Serializer.Serialize(message)); }
+                    catch { break; }
+                } while (true);
+
+                Console.WriteLine("Server disconnected");
+            }
+        }
+    }
+}
---
+++ easy_save.Desktop/Client.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace easy_save.Desktop
+{
+    public class Client : IDisposable
+    {
+        private bool closed;
+        private readonly Socket socket;
+
+        public event EventHandler Disposed;
+
+        public Client(Socket socket)
+        {
+            this.socket = socket;
+        }
+
+        public void Start()
+        {
+            Thread thread = new Thread(Listen);
+            thread.Start();
+        }
+
+        private void Listen()
+        {
+            byte[] buffer = new byte[1024];
+
+            while (!closed)
+            {
+                try
+                {
+                    int count = socket.Receive(buffer);
+                    //socket.Send(Serializer.Serialize("Message bien reçu"));
+                    if (count == 0)
+                        throw new SocketException();
+                }
+                catch (SocketException)
+                {
+                    Dispose();
+                }
+            }
+        }
+
+
+        private bool disposedValue;
+
+        protected virtual void Dispose(bool disposing)
+        {
+            if (!disposedValue)
+            {
+                if (disposing)
+                {
+                    socket.Dispose();
+                }
+                disposedValue = true;
+            }
+        }
+
+        public void Dispose()
+        {
+            if (closed)
+                return;
+            closed = true;
+            Disposed?.Invoke(this, EventArgs.Empty);
+
+            Dispose(disposing: true);
+            GC.SuppressFinalize(this);
+        }
+
+    }
+}
