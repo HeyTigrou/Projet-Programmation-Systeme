4d15cc6bad8bb3b162f499e0b326de89d0399d96
LukenIrazoqui <123357726+LukenIrazoqui@users.noreply.github.com>
2023-02-22 15:55:54 +0100

    Ajout du remoteeasySave

---
+++ RemoteEasySave.Service/Models/SaveWorkModel.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Runtime.Serialization;
+using System.Text;
+using System.Text.Json.Serialization;
+using System.Threading.Tasks;
+using System.Xml.Serialization;
+
+namespace RemoteEasySave.Lib.Models
+{
+    public enum SaveWorkState
+    {
+        Unstarted,
+        Running,
+        Done,
+        Paused,
+        Paused_ApplicationIsRunning
+    }
+    /// <summary>
+    /// This Model is used to manage the save works
+    /// </summary>
+    public class SaveWorkModel : INotifyPropertyChanged
+    {
+        public string Name { get; set; }
+        public string InputPath { get; set; }
+        public string OutputPath { get; set; }
+        public int SaveType { get; set; }
+
+
+        public string Progression { 
+            get { return (progression != null) ? progression : "" ; } 
+            set { progression = value; OnPropertyChanged("Progression"); } 
+        }
+
+        public string State
+        {
+            get { return (state != null) ? state : nameof(SaveWorkState.Unstarted); }
+            set { state = value; OnPropertyChanged("State"); }
+        }
+
+        private string progression;
+        private string state;
+
+        public event PropertyChangedEventHandler PropertyChanged;
+        
+        private void OnPropertyChanged(string propertyName)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+    }
+}
---
+++ RemoteEasySave/MainWindow.xaml.cs
@@ -0,0 +1,43 @@
+﻿using RemoteEasySave.Desktop.MVVM.ViewModel;
+using System;
+using System.Windows;
+
+namespace RemoteEasySave.Desktop
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+            SwitchLanguage("en");
+            SavesViewModel viewModel = new SavesViewModel();
+        }
+
+        private void FrenchButton_Checked(object sender, RoutedEventArgs e)
+        {
+            SwitchLanguage("fr");
+        }
+
+        private void EnglishButton_Checked(object sender, RoutedEventArgs e)
+        {
+            SwitchLanguage("en");
+        }
+
+        private void SwitchLanguage(string languageCode)
+        {
+            ResourceDictionary dict = new ResourceDictionary
+            {
+                Source = languageCode switch
+                {
+                    "en" => new Uri(@"..\Languages\LanguageEn.xaml", UriKind.Relative),
+                    "fr" => new Uri(@"..\Languages\LanguageFr.xaml", UriKind.Relative),
+                }
+            };
+            Resources.MergedDictionaries.Add(dict);
+        }
+
+    }
+}
